0254509fe22e0c9369e24f375c9a46a0e54f69b0
joseph sagiv <jhsagiv@gmail.com>
2020-11-18 13:06:06 +0200

    Remove logging.

--- lib/src/main/java/me/ibrahimsn/lib/PhoneNumberKit.kt
+++ lib/src/main/java/me/ibrahimsn/lib/PhoneNumberKit.kt
@@ -4,7 +4,6 @@ import android.content.Context
 import android.graphics.drawable.Drawable
 import android.text.InputFilter
 import android.text.InputType
-import android.util.Log
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
@@ -125,16 +124,13 @@ class PhoneNumberKit(private val context: Context) {
             }
 
             core.formatPhoneNumber(core.getExampleNumber(country.iso2))?.let { number ->
-
                 val formatNumber = if (formatWithCountryCode) {
                     number
                 } else {
                     number.removePrefix("+${country.countryCode}$CHAR_SPACE")
                 }
-
                 input?.editText?.filters = arrayOf(InputFilter.LengthFilter(formatNumber.length))
                 format = createNumberFormat(formatNumber)
-                Log.d("formatPhoneNumber", format)
                 applyFormat()
             }
         }
